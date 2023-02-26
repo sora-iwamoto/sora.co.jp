aee8dc6c018d2005f16aa5e6b85207d69e1a87a1
Sora Iwamoto <sorasky1309@gmail.com>
2023-02-26 16:51:50 +0900

    メニューバー作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,19 +15,36 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <!-- CSS -->
+    <link href="{{ asset('css/header.css') }}" rel="stylesheet">
+   
+
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                    <img src={{ asset('img/shalan_logo.png') }} alt="shalan" class="menuTopIcon">
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <a href="{{ url('/home') }}">
+                    <img src={{ asset('img/homebutton_negate.png') }} alt="Home" class="menuIcon" id="img1-before">
+                    <img src={{ asset('img/homebutton.png') }} alt="Home" class="menuIcon" id="img1-after">
+                </a>
+                 <a href="">
+                    <img src={{ asset('img/calenderbutton_negate.png') }} alt="Message" class="menuIcon" id="img2-before">
+                    <img src={{ asset('img/calenderbutton.png') }} alt="Message" class="menuIcon" id="img2-after">
+                </a>
+                <a href="">
+                    <img src={{ asset('img/messagebutton_negate.png') }} alt="Calender" class="menuIcon" id="img3-before">
+                    <img src={{ asset('img/messagebutton.png') }} alt="Calender" class="menuIcon" id="img3-after">
+                </a>
+                <a href="">
+                    <img src={{ asset('img/mypagebutton_negate.png') }} alt="Mypage" class="menuIcon" id="img4-before">
+                    <img src={{ asset('img/mypagebutton.png') }} alt="Mypage" class="menuIcon" id="img4-after">
+                </a>
+                <div class="navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
 
